1c81326f04658491b5f1f2fdec6ce6acc98f941c
perasperaadastra27 <aryabowo333@gmail.com>
2026-06-25 14:41:24 +0700

    [Tahap 4&5] Implementasi Inheritance dan Polimorphism Overriding

--- TAHAP 4 DAN 5/MahasiswaMandiri.php
+++ TAHAP 4 DAN 5/MahasiswaMandiri.php
@@ -4,16 +4,9 @@ class MahasiswaMandiri extends Mahasiswa {
 
     public function __construct($id, $nama, $nim, $sem, $tarif, $gol, $wali) {
         parent::__construct($id, $nama, $nim, $sem, $tarif);
-        $this->golonganUkt = $gol;
-        $this->namaWali = $wali;
+        $this->golonganUkt = $gol; $this->namaWali = $wali;
     }
 
-    // Overriding: Logika khusus Mandiri
-    public function hitungtagihansemester() {
-        return $this->tarif_ukt_nominal + 100000;
-    }
-
-    public function tampilkanspesifikasiakademik() {
-        return "Mandiri | Golongan: {$this->golonganUkt} | Wali: {$this->namaWali}";
-    }
+    public function hitungtagihansemester() { return $this->tarif_ukt_nominal + 100000; }
+    public function tampilkanspesifikasiakademik() { return "Mandiri (Gol: {$this->golonganUkt}, Wali: {$this->namaWali})"; }
 }
